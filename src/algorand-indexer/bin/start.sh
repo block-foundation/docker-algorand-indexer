7ac48059f9fdd96079c9d248f526a7a71ed88dcb
Lars van Vianen <lars@vianen.com>
2023-08-11 09:37:12 +0200

    Update start.sh

--- src/algorand-indexer/bin/start.sh
+++ src/algorand-indexer/bin/start.sh
@@ -4,43 +4,34 @@
 # environment variables.
 #
 # Configuration:
-#   INDEXER_PORT      - port to start indexer on.
-#   ALGOD_HOST        - host to connect to for algod.
-#   ALGOD_PORT        - port to connect to for algod.
-#   ALGOD_TOKEN       - token to use when connecting to algod.
-#   POSTGRES_HOST     -
-#   POSTGRES_PORT     -
-#   POSTGRES_USER     -
-#   POSTGRES_PASSWORD -
-#   POSTGRES_DB       -
-set -eu
+#   DISABLED          - If set start a server that returns an error instead of indexer.
+#   CONNECTION_STRING - the postgres connection string to use.
+#   PORT              - port to start indexer on.
+set -e
 
-echo "Starting indexer against algod."
+THIS=$(basename "$0")
 
-ALGOD_ADDR="${ALGOD_HOST}:${ALGOD_PORT}"
+disabled() {
+  echo "$THIS: running disabled.go script in lieu of indexer"
+  go run /tmp/disabled.go -port "$PORT" -code 200 -message "Indexer disabled for this configuration."
+}
 
-for i in 1 2 3 4 5 6 7 8 9 10; do
-  wget "${ALGOD_ADDR}/genesis" -O genesis.json && break
-  echo "Algod not responding... waiting."
-  sleep 10
-done
+echo "$THIS: starting indexer container script"
 
-if [ ! -f genesis.json ]; then
-  echo "Failed to create genesis file!"
+# Make sure data directory is available in case we're using a version that requires it.
+export INDEXER_DATA=/tmp/indexer-data
+mkdir -p ${INDEXER_DATA}
+
+if [ -n "$DISABLED" ]; then
+  disabled
+elif [ ! -f /tmp/algorand-indexer ]; then
+  echo "$THIS: binary not found at /tmp/algorand-indexer"
   exit 1
+else
+  /tmp/algorand-indexer daemon \
+    --dev-mode \
+    --server ":$PORT" \
+    -P "$CONNECTION_STRING"
 fi
 
-for i in 1 2 3 4 5 6 7 8 9 10; do
-  nc -z $POSTGRES_HOST $POSTGRES_PORT && break
-  echo "Waiting for postgres to come alive..."
-  sleep 5
-done
-
-echo "All services alive, starting indexer daemon"
-exec /tmp/algorand-indexer daemon \
-  --dev-mode \
-  --server ":${INDEXER_PORT}" \
-  -P "host=${POSTGRES_HOST} port=${POSTGRES_PORT} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD} dbname=${POSTGRES_DB} sslmode=disable" \
-  --algod-net "${ALGOD_ADDR}" \
-  --algod-token "${ALGOD_TOKEN}" \
-  --genesis "genesis.json"
+sleep infinity
